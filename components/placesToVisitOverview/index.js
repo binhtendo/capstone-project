df159a1cbdc0c303eded357e3251ae46a9cb669a
Binh Nguyen <db.nguyen@yahoo.com>
2023-03-22 10:50:26 +0100

    provisorisch

--- components/placesToVisitOverview/index.js
+++ components/placesToVisitOverview/index.js
@@ -8,36 +8,27 @@ export default function PlacesToVisitOverview() {
   const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
-    if (searchTerm.length === 0) {
-      return;
-    }
-    apiGet("geoname", "name=" + searchTerm + "&limit=1")
-    //   .then((data) => {
-        // const lat = data[0].lat;
-        // const lon = data[0].lon;
-        // apiGet("radius", `radius=10000&lon=${lon}&lat=${lat}&type=sightseeing`)
-          .then((data) => {
-            const sortedData = data.features
-              .filter((place) => place.properties.rate >= 4)
-              .sort((a, b) => b.properties.rate - a.properties.rate);
-            const uniqueData = new Set(
-              sortedData.map((place) => place.properties.name)
-            );
-            const placesWithFavorite = [...uniqueData].map((name) => {
-              const place = sortedData.find(
-                (place) => place.properties.name === name
-              );
-              return {
-                ...place,
-                isFavorite: false,
-              };
-            });
-            setPlaces(placesWithFavorite);
-          })
-          .catch((err) => console.log("Fetch Error :-S", err));
+    apiGet("radius", "radius=10000&lon=9.99302&lat=53.55073&type=sightseeing")
+      .then((data) => {
+        const sortedData = data.features
+          .filter((place) => place.properties.rate >= 4)
+          .sort((a, b) => b.properties.rate - a.properties.rate);
+        const uniqueData = new Set(
+          sortedData.map((place) => place.properties.name)
+        );
+        const placesWithFavorite = [...uniqueData].map((name) => {
+          const place = sortedData.find(
+            (place) => place.properties.name === name
+          );
+          return {
+            ...place,
+            isFavorite: false,
+          };
+        });
+        setPlaces(placesWithFavorite);
       })
       .catch((err) => console.log("Fetch Error :-S", err));
-  }, [searchTerm]);
+  }, []);
 
   const toggleFavorite = (xid) => {
     setPlaces(
@@ -65,18 +56,16 @@ export default function PlacesToVisitOverview() {
     <div>
       <h2>Results:</h2>
       <Searchbox value={searchTerm} onChange={handleSearch} />
-      {searchTerm.length > 0 && (
-        <ul>
-          {filteredPlaces.map((place, index) => (
-            <Place
-              key={place.properties.xid}
-              name={place.properties.name}
-              onFavorite={toggleFavorite}
-              xid={place.properties.xid}
-            />
-          ))}
-        </ul>
-      )}
+      <ul>
+        {filteredPlaces.map((place, index) => (
+          <Place
+            key={place.properties.xid}
+            name={place.properties.name}
+            onFavorite={toggleFavorite}
+            xid={place.properties.xid}
+          />
+        ))}
+      </ul>
     </div>
   );
 }
